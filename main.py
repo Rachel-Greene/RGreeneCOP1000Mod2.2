39489dc7aebf8683e4c9c60afe79a3c41f71c0b7
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-02-22 14:50:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# This program calculates your age in the year 2050.
+
+myCurrentAge = 32
+currentYear = 2024
+
+myNewAge = myCurrentAge + (2050 - currentYear)
+print("My Current Age is " + str(myCurrentAge))
+print("I will be " + str(myNewAge) + " in 2050.")
